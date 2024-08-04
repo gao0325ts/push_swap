160ec3948f1a081ac90a36c38a827fbf7d4d5e84
stakada <stakada@student.42tokyo.jp>
2024-08-04 14:38:59 +0900

    add main

---
+++ srcs/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 17:02:52 by stakada           #+#    #+#             */
+/*   Updated: 2024/08/04 14:22:46 by stakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "init.h"
+// #include "sort.h"
+
+int	main(int ac, char **av)
+{
+	t_stack	**stack_a;
+	t_stack	**stack_b;
+
+	check_args(ac, av);
+	// stack_a = init_a(ac, av);
+	// stack_b = init_b(ac, av);
+	return (0);
+}
