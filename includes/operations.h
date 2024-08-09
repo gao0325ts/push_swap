6c42080d39849b16428bb49185e4021af4d311dc
stakada <stakada@student.42tokyo.jp>
2024-08-09 12:28:10 +0900

    rename and update operations.h

--- includes/operations.h
+++ includes/operations.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   oparations.h                                       :+:      :+:    :+:   */
+/*   operations.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/06 03:00:17 by stakada           #+#    #+#             */
-/*   Updated: 2024/08/06 03:10:20 by stakada          ###   ########.fr       */
+/*   Updated: 2024/08/09 12:28:24 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,15 @@ void sa(t_stack **a);
 void sb(t_stack **b);
 void ss(t_stack **a, t_stack **b);
 
+void    pa(t_stack **a, t_stack **b);
+void    pb(t_stack **a, t_stack **b);
+
 void    ra(t_stack **a);
 void   rb(t_stack **b);
 void    rr(t_stack **a, t_stack **b);
+
+void    rra(t_stack **a);
+void   rrb(t_stack **b);
+void    rrr(t_stack **a, t_stack **b);
 
 #endif
