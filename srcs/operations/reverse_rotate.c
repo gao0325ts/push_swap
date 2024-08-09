543703f5382dff93b32866cf02ea3ec7ee1a33ee
stakada <stakada@student.42tokyo.jp>
2024-08-09 12:25:28 +0900

    add rra, rrb, rrr

---
+++ srcs/operations/reverse_rotate.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 11:35:24 by stakada           #+#    #+#             */
+/*   Updated: 2024/08/09 12:07:11 by stakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "operations.h"
+
+void rra(t_stack **a)
+{
+    t_stack *last;
+    t_stack *second_last;
+
+    if (!*a || !(*a)->next)
+        return;
+    last = *a;
+    second_last = NULL; 
+    while (last->next)
+    {
+        second_last = last;
+        last = last->next;
+    }
+    second_last->next = NULL;
+    last->next = *a;
+    *a = last;
+}
+
+void rrb(t_stack **b)
+{
+    t_stack *last;
+    t_stack *second_last;
+
+    if (!*b || !(*b)->next)
+        return;
+    last = *b;
+    second_last = NULL;
+    while (last->next)
+    {
+        second_last = last;
+        last = last->next;
+    }
+    second_last->next = NULL;
+    last->next = *b;
+    *b = last;
+}
+
+void rrr(t_stack **a, t_stack **b)
+{
+    rra(a);
+    rrb(b);
+}
+
+// #include <stdio.h>
+// #include "init.h"
+
+// void print_stack(t_stack *stack)
+// {
+//     while (stack)
+//     {
+//         printf("( [%d] %d ) -> ", stack->coord, stack->value);
+//         stack = stack->next;
+//     }
+//     printf("NULL\n");
+// }
+
+// int main(int ac, char **av)
+// {
+//     t_stack **a = init_a(ac, av);
+//     t_stack **b = init_a(ac, av);
+//     t_stack **a2 = init_a(ac, av);
+//     t_stack **b2 = init_a(ac, av);
+
+//     puts("-----rra-----");
+//     puts("Before:");
+//     print_stack(*a);
+//     rra(a);
+//     puts("Atfter:");
+//     print_stack(*a);
+
+//     puts("-----rrb-----");
+//     puts("Before:");
+//     print_stack(*b);    
+//     rrb(b);
+//     puts("After:");
+//     print_stack(*b);
+
+//     puts("-----rrr-----");
+//     puts("Before:");
+//     print_stack(*a2);
+//     print_stack(*b2);
+//     rrr(a2, b2);
+//     puts("After:");
+//     print_stack(*a2);
+//     print_stack(*b2);
+
+//     clear_nodes(a);
+//     free(a);
+//     clear_nodes(b);
+//     free(b);
+//     clear_nodes(a2);
+//     free(a2);
+//     clear_nodes(b2);
+//     free(b2);
+//     return 0;
+// }
